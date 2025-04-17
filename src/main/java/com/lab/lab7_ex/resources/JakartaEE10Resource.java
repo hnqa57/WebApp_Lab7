bfa8985faad6cd35651a91d214016f19544adfbd
Quỳnh Anh <quynhanhhoang572004@gmail.com>
2025-04-17 13:02:32 +0700

    first init

---
+++ src/main/java/com/lab/lab7_ex/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.lab.lab7_ex.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
---
+++ src/main/java/com/university/model/Course.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.university.model;
+
+/**
+ *
+ * @author PC
+ */
+public class Course {
+
+    public Course(int id, String code, String description, int credits, String name) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.credits = credits;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int id;
+    private String code;
+    private String description;
+    private int credits;
+    private String name;
+
+    public Course() {
+
+    }
+
+   
+}
